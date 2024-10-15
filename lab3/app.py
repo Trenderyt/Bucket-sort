46108491aabf3efa5d1e573983fec1b9bc1cd904
Nik-evvron <nikita.kylabin@yandex.ru>
2024-10-15 03:27:15 +0300

    ааааааа

--- lab3/app.py
+++ lab3/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, render_template
-from random import *
 import sqlite3
+from random import *
+
+from flask import Flask, request, render_template, redirect
+
 from __init__ import *
 
 # Настройка приложения Flask
 app = Flask(__name__)
+
 
 # Создание базы данных и таблицы
 def init_db():
@@ -21,6 +24,7 @@ def init_db():
     conn.commit()
     conn.close()
 
+
 # Сохранение массива в БД
 def save_array(original_array, sorted_array, block_size):
     conn = sqlite3.connect('arrays.db')
@@ -32,13 +36,16 @@ def save_array(original_array, sorted_array, block_size):
     conn.commit()
     conn.close()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/process', methods=['POST'])
 def process():
     input_type = request.form.get('input_type')
+    print("Input Type: ", input_type)  # Проверка типа ввода
     arr = []
     block_size = 0
     sorted_array = []
@@ -49,16 +56,19 @@ def process():
     elif input_type == 'random':
         n = randint(3, 20)
         block_size = randint(1, n)
-        arr = [uniform(-100, 100) for i in range(n)]
+        arr = [uniform(-100, 100) for _ in range(n)]
         arr = [round(num, 2) for num in arr]
+
+    print("Generated Array: ", arr)  # Проверка сгенерированного массива
 
     sorted_array = block_sort(arr, block_size)
 
-    # Сохранение данных в БД
+    print("Sorted Array: ", sorted_array)  # Проверка отсортированного массива
+
     save_array(arr, sorted_array, block_size)
 
-    # Отправляем результат в шаблон
     return render_template('result.html', original=arr, sorted=sorted_array, block_size=block_size)
+
 
 @app.route('/history')
 def history():
@@ -69,6 +79,17 @@ def history():
     conn.close()
 
     return render_template('history.html', rows=rows)
+
+
+@app.route('/clear_history', methods=['POST'])
+def clear_history():
+    conn = sqlite3.connect('arrays.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM arrays')
+    conn.commit()
+    conn.close()
+    return redirect('/history')
+
 
 if __name__ == '__main__':
     init_db()
